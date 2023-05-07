937304c83a1741e972dde3b056eb8ce5bd856378
EngCuadrado <cuadradoengcs50@gmail.com>
2023-05-06 19:30:37 -0600

    getch letras

--- ciclos/letras.C
+++ ciclos/letras.C
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <conio.h>
+#include <dos.h>
 
 int main(void)
 {
-    
+
 char letra = 'l';
 int contador = 0;
 clrscr();
 
     while(letra != 'n')
     {
-
+		/*letra = '';*/
         printf("Escribe una Letra: ");
-        scanf("%c", &letra);
+        letra = getch();
+        printf("%c \n", letra);
         contador++;
+        delay(300);
     }
 
     printf("\n\n\nSe han escrito %d ",contador);
@@ -22,4 +25,4 @@ clrscr();
 
     getch();
     return 0;
-}
+}
